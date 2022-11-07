17f2d213ba9c9e59874c69b7171e0f008009ce76
Anthony Proctor <anthonyproctor@gmail.com>
2022-11-07 12:47:34 -0700

    Added expense date mod

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -2,6 +2,12 @@ function ExpenseDate(props) {
   const month = props.date.toLocalString('en-US', { month: 'long'});
   const day = props.date.toLocalString('en-US', { day: '2-digit'});
   const year = props.date.getFullYear();
+
+  <div>
+    <div>{month}</div>
+    <div>{year}</div>
+    <div>{day}</div>
+  </div>
 }
 
 export default ExpenseDate;
