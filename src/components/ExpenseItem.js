ab8096500991843a8117bb5b15de154ba1c5ab72
Anthony Proctor <anthonyproctor@gmail.com>
2022-11-07 10:01:23 -0700

    added date, time and item expense

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 10, 4);
+  const expenseTitle = 'Care Insurance';
+  const expenseAmount = 294.67;
+
   return (
     <div className='expense-item'>
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Title</h2>
-        <div className='expense-item__price'>Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
